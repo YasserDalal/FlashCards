e6e636e464dd08a13fb56e1a0168521acc5cf298
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-29 18:02:01 +0300

    make the buttons interactive,

--- src/shared/components/ButtonContainer.jsx
+++ src/shared/components/ButtonContainer.jsx
@@ -1,16 +1,16 @@
 
-export default function ButtonContainer(){
+export default function ButtonContainer({ questionsAndAnswers, nextButton, previousButton,  setNext, setCounter, counter, showAndHideButton, setSmooth, smooth, colorPrev, colorNext }) {
   return (
     <div className="h-[10%]">
       <div className="flex rounded-lg justify-between items-center bg-slate-600 h-full px-3">
-        <div className="flex">
+        <div className={`flex ${colorPrev && "text-slate-400"} `} onClick={() => previousButton(setNext, counter, setCounter, smooth, setSmooth)}>
           <div className="mr-2">&lt;</div>
           <div>Previous</div>
         </div>
-        <div>
-          <div className="text-lg">Show Answer</div>
+        <div onClick={() => showAndHideButton(setSmooth, smooth)}>
+          <div className="text-lg">{smooth === 360 ? "Show Answer" : "Hide Answer"}</div>
         </div>
-        <div className="flex">
+        <div className={`flex ${colorNext && "text-slate-400"}`} onClick={() => nextButton(setNext, counter, setCounter, questionsAndAnswers, smooth, setSmooth)}>
           <div>Next</div>
           <div className="ml-2">&gt;</div>
         </div>
