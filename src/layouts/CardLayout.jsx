9a8fe0de8b9004f59fdb1204afe910032175d075
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-29 18:01:28 +0300

    pass all the states, data and events as a props to the CardLayout component

--- src/layouts/CardLayout.jsx
+++ src/layouts/CardLayout.jsx
@@ -4,16 +4,15 @@ import ProgressContent from "../shared/components/ProgressContent"
 import MainContent from "../shared/components/MainContent"
 import ButtonContainer from "../shared/components/ButtonContainer"
 
-
-export default function MainLayout(){
+export default function CardLayout({ questionsAndAnswers, nextButton, setNext, next, previousButton, setCounter, counter, showAnswer, setShowAnswer, smooth, setSmooth, showAndHideButton, colorPrev, setColorPrev, colorNext }) {
   return (
     <div className="flex justify-center items-center h-full px-5">
       <div className="max-w-[650px] min-w-[200px] w-full h-[650px] flex flex-col px-5 py-5 gap-3">
         <Header />
         <ProgressContent />
         <div className="h-full rounded-xl mt-3 border p-2">
-          <MainContent />
-          <ButtonContainer />
+          <MainContent questionsAndAnswers={questionsAndAnswers} next={next} smooth={smooth} showAnswer={showAnswer}/>
+          <ButtonContainer questionsAndAnswers={questionsAndAnswers} nextButton={nextButton} previousButton={previousButton} setNext={setNext} setCounter={setCounter} counter={counter} setShowAnswer={setShowAnswer} setSmooth={setSmooth} showAndHideButton={showAndHideButton} smooth={smooth} colorPrev={colorPrev} setColorPrev={setColorPrev} colorNext={colorNext}/>
         </div> 
       </div>
     </div>
