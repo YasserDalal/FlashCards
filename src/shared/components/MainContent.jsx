e3d4fb83be8803c4e7fd26e917541a05bce75b48
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-29 18:02:36 +0300

    render the data from array of objects

--- src/shared/components/MainContent.jsx
+++ src/shared/components/MainContent.jsx
@@ -1,9 +1,16 @@
 
-export default function MainContent() {
+export default function MainContent({ questionsAndAnswers, next, smooth, showAnswer }) {
   return (
-    <div className="h-[88%] mb-2">
-      <div className="flex rounded-lg justify-center bg-slate-600 h-full">
-        <h2>MainContent</h2>
+    <div className="h-[88%] max-w-[592px] min-w-[200px] w-full mb-2 overflow-hidden relative">
+      <div className="h-full flex w-[300%] transition-transform duration-300 gap-[2px]" style={{ transform: `translateX(-${next}%)` }}>
+        {questionsAndAnswers.map((item, index) => (
+          <div className="w-full flex rounded-lg justify-center items-center bg-slate-600 relative" key={index}>
+            <h2>{item.question}</h2>
+            <div className="flex items-center transition-transform duration-300 text-center bg-slate-600 absolute z-10 top-0 bottom-0 left-0 right-0" style={{ transform: `translateY(${smooth}%)` }}>
+              {showAnswer && <h3 className="w-full">{item.answer}</h3>}
+            </div>
+          </div>
+        ))}    
       </div>
     </div>
   )
