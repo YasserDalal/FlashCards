b0e7af22c0c1363308e385388114749e1185c13f
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-29 14:44:20 +0300

    add the questions and answers data

---
+++ src/data/questionsAndAnswers.js
@@ -0,0 +1,15 @@
+
+export default questionsAndAnswers = [
+  {
+    question: "What is the difference between var, let, and const",
+    answer: "In Javascript, var is function-scoped and can be re-declared; let and const are block-scoped, with let allowing re-assigment and const preventing it, However, const objects can have their contents modified."
+  },
+  {
+    question: "What is the capital of Italy?",
+    answer: "Rome"
+  },
+  {
+    question: "What is the capital of Spain?",
+    answer: "Madrid"
+  }
+]
