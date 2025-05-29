947882b1c45cdc825b343c7f6de479014b288d41
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-29 17:57:16 +0300

    create an array of objects containing Q and A

--- src/data/questionsAndAnswers.js
+++ src/data/questionsAndAnswers.js
@@ -1,5 +1,5 @@
 
-export default questionsAndAnswers = [
+export const questionsAndAnswers = [
   {
     question: "What is the difference between var, let, and const",
     answer: "In Javascript, var is function-scoped and can be re-declared; let and const are block-scoped, with let allowing re-assigment and const preventing it, However, const objects can have their contents modified."
