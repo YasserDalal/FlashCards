74d50d7e4e1b19bc4cf7a5c54301345b8ace0aea
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-29 18:19:47 +0300

    add cursor pointer to buttons

--- src/shared/components/ButtonContainer.jsx
+++ src/shared/components/ButtonContainer.jsx
@@ -3,14 +3,14 @@ export default function ButtonContainer({ questionsAndAnswers, nextButton, previ
   return (
     <div className="h-[10%]">
       <div className="flex rounded-lg justify-between items-center bg-slate-600 h-full px-3">
-        <div className={`flex ${colorPrev && "text-slate-400"} `} onClick={() => previousButton(setNext, counter, setCounter, smooth, setSmooth)}>
+        <div className={`flex ${colorPrev && "text-slate-400"} cursor-pointer`} onClick={() => previousButton(setNext, counter, setCounter, smooth, setSmooth)}>
           <div className="mr-2">&lt;</div>
           <div>Previous</div>
         </div>
-        <div onClick={() => showAndHideButton(setSmooth, smooth)}>
+        <div className="cursor-pointer" onClick={() => showAndHideButton(setSmooth, smooth)}>
           <div className="text-lg">{smooth === 360 ? "Show Answer" : "Hide Answer"}</div>
         </div>
-        <div className={`flex ${colorNext && "text-slate-400"}`} onClick={() => nextButton(setNext, counter, setCounter, questionsAndAnswers, smooth, setSmooth)}>
+        <div className={`flex ${colorNext && "text-slate-400"} cursor-pointer`} onClick={() => nextButton(setNext, counter, setCounter, questionsAndAnswers, smooth, setSmooth)}>
           <div>Next</div>
           <div className="ml-2">&gt;</div>
         </div>
