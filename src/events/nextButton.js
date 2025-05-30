b8676e65f532c0ab00ab609d019d71ade0ad530e
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-30 17:47:49 +0300

    make the buttons dynamic when interaction occurs

--- src/events/nextButton.js
+++ src/events/nextButton.js
@@ -1,7 +1,10 @@
 
-export default function nextButton(setNext, counter, setCounter, questionsAndAnswers, smooth, setSmooth) {
+export default function nextButton(setNext, counter, setCounter, questionsAndAnswers, smooth, setSmooth, setProgress, itemOrder, setItemOrder) {
   if(counter === questionsAndAnswers.length - 1) return;
   if(smooth === 0) setSmooth(prev => prev + 360);
-  setNext((prev) => prev + 33.35)
+  if(itemOrder === questionsAndAnswers.length) return;
+  setProgress(prev => prev + (100 / questionsAndAnswers.length))
+  setNext((prev) => prev + (100 / questionsAndAnswers.length))
   setCounter(prev => prev + 1)
+  setItemOrder(prev => prev + 1)
 }
--- src/events/previousButton.js
+++ src/events/previousButton.js
@@ -1,7 +1,10 @@
 
-export default function previousButton(setNext, counter, setCounter, smooth, setSmooth) {
+export default function previousButton(setNext, counter, setCounter, smooth, setSmooth, questionsAndAnswers, setProgress, itemOrder, setItemOrder) {
   if(counter <= 0) return;
   if(smooth === 0) setSmooth(prev => prev + 360);
+  if(itemOrder === 1) return;
   setCounter(prev => prev - 1)
-  setNext(prev => prev - 33.35)
+  setNext(prev => prev - (100 / questionsAndAnswers.length))
+  setProgress(prev => prev - (100 / questionsAndAnswers.length))
+  setItemOrder(prev => prev - 1)
 }
