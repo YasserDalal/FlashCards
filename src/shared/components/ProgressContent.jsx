4afe3f049c066c4a6e05aad9c0dc33f6a39c8df3
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-30 17:48:27 +0300

    make the ProgressContent component interactive and dynamic

--- src/shared/components/ProgressContent.jsx
+++ src/shared/components/ProgressContent.jsx
@@ -1,13 +1,16 @@
 
-export default function ProgressContent(){
+export default function ProgressContent({ progress, questionsAndAnswers, itemOrder }) {
   return (
     <div> 
-      <div className="flex justify-between items-center gap-1 border-2 border-slate-200 py-1 px-[5px] rounded-md">
-        <div className="flex items-center gap-1">
-          <div className="w-32 h-5 bg-slate-400 rounded-md"></div>
-          <div>30%</div>
+      <div className="flex justify-between items-center gap-1 border-2 border-slate-500 py-1 px-[5px] rounded-md relative">
+        <div className="flex items-center gap-1 w-full">
+          <div className='h-5 bg-slate-400 rounded-md relative' style={{ width: `${progress}%` }}>
+            <div className="absolute -right-12 -top-1">
+              <div>{Math.floor(progress) >= 100 ? 100 : Math.floor(progress)}%</div>
+            </div>
+          </div>
         </div>
-        <div className="text-sm">5 of 20</div>
+        <div className="text-sm absolute -top-6 right-1">{itemOrder} of {questionsAndAnswers.length}</div>
       </div>
     </div>
   )
