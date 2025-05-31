6c29c23a548f8b351914a683eb2e677a459ca61d
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-31 16:09:35 +0300

    add type attribute to divs for SEO concerns

--- src/shared/components/ButtonContainer.jsx
+++ src/shared/components/ButtonContainer.jsx
@@ -3,14 +3,14 @@ export default function ButtonContainer({ questionsAndAnswers, nextButton, previ
   return (
     <div className="h-[10%]">
       <div className="flex rounded-lg justify-between items-center bg-slate-600 h-full px-3">
-        <div className={`flex ${colorPrev && "text-slate-400"} cursor-pointer select-none`} onClick={() => previousButton(setNext, counter, setCounter, smooth, setSmooth, questionsAndAnswers, setProgress, itemOrder, setItemOrder)}>
+        <div className={`flex ${colorPrev && "text-slate-400"} cursor-pointer select-none`} type="button" onClick={() => previousButton(setNext, counter, setCounter, smooth, setSmooth, questionsAndAnswers, setProgress, itemOrder, setItemOrder)}>
           <div className="mr-2">&lt;</div>
           <div>Previous</div>
         </div>
-        <div className="cursor-pointer select-none" onClick={() => showAndHideButton(setSmooth, smooth)}>
+        <div className="cursor-pointer select-none" type="button" onClick={() => showAndHideButton(setSmooth, smooth)}>
           <div className="text-lg">{smooth === 360 ? "Show Answer" : "Hide Answer"}</div>
         </div>
-        <div className={`flex ${colorNext && "text-slate-400"} cursor-pointer select-none`} onClick={() => nextButton(setNext, counter, setCounter, questionsAndAnswers, smooth, setSmooth, setProgress, itemOrder, setItemOrder)}>
+        <div className={`flex ${colorNext && "text-slate-400"} cursor-pointer select-none`} type="button" onClick={() => nextButton(setNext, counter, setCounter, questionsAndAnswers, smooth, setSmooth, setProgress, itemOrder, setItemOrder)}>
           <div>Next</div>
           <div className="ml-2">&gt;</div>
         </div>
