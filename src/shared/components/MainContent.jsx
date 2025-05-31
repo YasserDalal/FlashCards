b42f38950c54a61d57e3afd737d0041cb7e34281
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-31 18:52:23 +0300

    make the text responsive  and add media queries

--- src/shared/components/MainContent.jsx
+++ src/shared/components/MainContent.jsx
@@ -5,9 +5,9 @@ export default function MainContent({ questionsAndAnswers, next, smooth, showAns
       <div className="h-full flex transition-transform duration-300 gap-[2px]" style={{ transform: `translateX(-${next}%)`,
     width: `${questionsAndAnswers.length}00%` }}>
         {questionsAndAnswers.map((item, index) => (
-          <div className="w-full flex rounded-lg justify-center items-center bg-slate-600 relative" key={index}>
+          <div className="w-full flex rounded-lg justify-center items-center bg-slate-600 relative text-[clamp(1rem,5vw,1.5rem)] text-center px-3" key={index}>
             <h2>{item.question}</h2>
-            <div className="flex items-center transition-transform duration-300 text-center bg-slate-600 absolute z-10 top-0 bottom-0 left-0 right-0" style={{ transform: `translateY(${smooth}%)` }}>
+            <div className="flex items-center transition-transform duration-300 text-center bg-slate-600 absolute z-10 top-0 bottom-0 left-0 right-0 px-3" style={{ transform: `translateY(${smooth}%)` }}>
               {showAnswer && <h3 className="w-full">{item.answer}</h3>}
             </div>
           </div>
