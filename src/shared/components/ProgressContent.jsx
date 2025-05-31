2da2d3844a98b5468eda19f0de1c2359642e2eb6
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-31 18:52:43 +0300

    adjust the position of percentage

--- src/shared/components/ProgressContent.jsx
+++ src/shared/components/ProgressContent.jsx
@@ -5,7 +5,7 @@ export default function ProgressContent({ progress, questionsAndAnswers, itemOrd
       <div className="flex justify-between items-center gap-1 border-2 border-slate-500 py-1 px-[5px] rounded-md relative">
         <div className="flex items-center gap-1 w-full">
           <div className='h-5 bg-slate-400 rounded-md relative' style={{ width: `${progress}%` }}>
-            <div className="absolute -right-12 -top-1">
+            <div className="absolute -right-0 top-6">
               <div>{Math.floor(progress) >= 100 ? 100 : Math.floor(progress)}%</div>
             </div>
           </div>
