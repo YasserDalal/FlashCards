3b38ffc1d6fca69654f9ad22d137fd685fa92790
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-25 20:11:07 +0300

    add semi colon, fix the code structure and indentations

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
+import { useState, useEffect } from "react";
 
-import { useState, useEffect} from "react"
+import CardLayout from "./layouts/CardLayout";
+import { questionsAndAnswers } from "./data/questionsAndAnswers";
 
-import CardLayout from "./layouts/CardLayout"
-import { questionsAndAnswers } from "./data/questionsAndAnswers"
-
-import nextButton from "./events/nextButton"
-import previousButton from "./events/previousButton"
-import showAndHideButton from "./events/showAndHideButton"
+import nextButton from "./events/nextButton";
+import previousButton from "./events/previousButton";
+import showAndHideButton from "./events/showAndHideButton";
 
 export default function App() {
-  const [next, setNext] = useState(0)
-  const [counter, setCounter] = useState(0)
-  const [showAnswer, setShowAnswer] = useState(false)
-  const [smooth, setSmooth] = useState(360)
-  const [colorPrev, setColorPrev] = useState(false)
-  const [colorNext, setColorNext] = useState(false)
-  const [progress, setProgress] = useState(20)
-  const [itemOrder, setItemOrder] = useState(1)
+  const [next, setNext] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [smooth, setSmooth] = useState(360);
+  const [colorPrev, setColorPrev] = useState(false);
+  const [colorNext, setColorNext] = useState(false);
+  const [progress, setProgress] = useState(20);
+  const [itemOrder, setItemOrder] = useState(1);
   // for transitioning hide and show for answers
   useEffect(() => {
     if (smooth === 0) {
-      setShowAnswer(true)
+      setShowAnswer(true);
     } else {
       const timeout = setTimeout(() => setShowAnswer(false), 300);
       return () => clearTimeout(timeout);
@@ -29,26 +28,41 @@ export default function App() {
   // for change the color of the buttons of 'previous' and 'next'
   useEffect(() => {
     if (counter === 0) {
-      setColorPrev(true)
-      setColorNext(false)
-    } 
-    if(counter === questionsAndAnswers.length - 1) {
-      setColorNext(true)
-      setColorPrev(false)
+      setColorPrev(true);
+      setColorNext(false);
+    }
+    if (counter === questionsAndAnswers.length - 1) {
+      setColorNext(true);
+      setColorPrev(false);
     }
     if (counter > 0 && counter < questionsAndAnswers.length - 1) {
-      setColorNext(false)
-      setColorPrev(false)
+      setColorNext(false);
+      setColorPrev(false);
     }
   }, [counter]);
   return (
-    <div className="h-screen max-w-full min-w-[320px] bg-slate-800 text-white">
-      <CardLayout questionsAndAnswers={questionsAndAnswers} 
-                  nextButton={nextButton} setNext={setNext} next={next}
-                  previousButton={previousButton} setCounter={setCounter} counter={counter}
-                  showAnswer={showAnswer} setShowAnswer={setShowAnswer} smooth={smooth} setSmooth={setSmooth}
-                  showAndHideButton={showAndHideButton} colorPrev={colorPrev} setColorPrev={setColorPrev} colorNext={colorNext}
-                  progress={progress} setProgress={setProgress} itemOrder={itemOrder} setItemOrder={setItemOrder}/>
+    <div className='h-screen max-w-full min-w-[320px] bg-slate-800 text-white'>
+      <CardLayout
+        questionsAndAnswers={questionsAndAnswers}
+        nextButton={nextButton}
+        setNext={setNext}
+        next={next}
+        previousButton={previousButton}
+        setCounter={setCounter}
+        counter={counter}
+        showAnswer={showAnswer}
+        setShowAnswer={setShowAnswer}
+        smooth={smooth}
+        setSmooth={setSmooth}
+        showAndHideButton={showAndHideButton}
+        colorPrev={colorPrev}
+        setColorPrev={setColorPrev}
+        colorNext={colorNext}
+        progress={progress}
+        setProgress={setProgress}
+        itemOrder={itemOrder}
+        setItemOrder={setItemOrder}
+      />
     </div>
-  )
+  );
 }
